e525dafe1a56a0fd3192206fed699788b6240494
Niclas Johansen <niclasjohansen@Niclass-MacBook-Pro.local>
2017-10-26 21:47:51 +0200

    Add door with methods. Needs integration with arraylist

---
+++ Semesterprojekt/src/worldofzuul/Furniture/Door.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldofzuul.Furniture;
+
+import worldofzuul.items.Item;
+import worldofzuul.items.Key;
+
+/**
+ *
+ * @author Niclas
+ */
+public class Door extends Furniture {
+    private boolean doorLocked;
+    private Item items[];
+        private KeyHole doorhole;
+    /**
+     * constructor for a door
+     * @param doorName
+     * @param doorDescription
+     * @param doorLocked
+     */
+    
+    public Door (String doorName, String doorDescription, boolean doorLocked ){
+        super.setFurnitureName(doorName);
+        super.setFurnitureDiscription(doorDescription);
+        this.doorLocked = doorLocked;
+        
+    }
+    
+    Door toiletDøren = new Door("Toiletdøren", "Ikke gå herind, der lugter", true);
+    
+    /**
+     * method for inspect in door
+     */
+    public KeyHole inspectKeyhole(KeyHole picture ){
+            return picture;
+    }
+    
+    /**
+     * method for checking if the door is locked
+     */
+    public void isDoorLocked(Key key, Item items[]){
+        if (items.contains(key)) {
+            System.out.println("You got the key! Door is now locked up Sir");
+            items.remove(key);
+        }
+        else {
+           System.out.println("Nahh. Door is locked");
+        }
+        
+    }
+    
+    
+}
