2fe201074011b8bf585f6c2778deb169fd22d3e0
Jonas Bjørstorp <jonas_46@msn.com>
2017-11-06 21:38:19 +0100

    Shells for saving methods

--- Semesterprojekt/src/worldofzuul/mapAndRooms/Map.java
+++ Semesterprojekt/src/worldofzuul/mapAndRooms/Map.java
@@ -1,5 +1,12 @@
 package worldofzuul.mapAndRooms;
 
+import java.util.ArrayList;
+import worldofzuul.Entity;
+import worldofzuul.Furniture.Furniture;
+import worldofzuul.People.Player;
+import worldofzuul.People.Student;
+import worldofzuul.items.Item;
+
 /**
  *
  * @author Niclas Johansen & Rasmus WIller & Søren Bendtsen
@@ -10,10 +17,56 @@ public class Map {
     private int exit;
     private String description;
     private boolean locked;
-    
+    // containers for entities
+    private ArrayList<Item> itemsInMap = new ArrayList<>();
+    private ArrayList<Player> playersInMap = new ArrayList<>();
+    private ArrayList<Student> studentsInMap = new ArrayList<>();
+    private ArrayList<Furniture> furnitureInMap = new ArrayList<>();
     public Map(){
         
     }
+
+    /**
+     *
+     * @param eList
+     * @param o
+     */
+    public void save() {
+        saveItems();
+        savePlayers();
+        saveStudents();
+        saveFurniture();
+    }
+    public void saveItems() {
+        for(Item i : itemsInMap) {
+            // save name to txt file
+            // save x coord to txt file
+            // save y coord to txt file
+            // save currentRoom
+        }
+        
+    }
+    public void savePlayers() {
+        for(Player p : playersInMap) {
+            
+        }
+    }
+    public void saveStudents() {
+        for(Student s : studentsInMap) {
+            
+        }
+    }
+    public void saveFurniture() {
+        for(Furniture f : furnitureInMap) {
+            
+        }
+    }
+
+    /**
+     *
+     * @param iList
+     */
+
 
     public int item() {
         return this.item;
